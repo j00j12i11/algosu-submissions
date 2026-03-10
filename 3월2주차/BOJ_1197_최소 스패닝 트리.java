31bdb5994ccecfdd778e79d63bcad9203e1f8215
JIIN <73988273+j00j12i11@users.noreply.github.com>
2026-03-10 17:19:30 +0900

    Submit: 최소 스패닝 트리 (java)

--- 3월2주차/BOJ_1197_최소 스패닝 트리.java
+++ 3월2주차/BOJ_1197_최소 스패닝 트리.java
@@ -1,55 +1,81 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	
+
+	static int V, E;
+	static long ANS;
 	static int[] p;
+	static Edge[] list;
+
+	static class Edge implements Comparable<Edge> {
+		int a, b, w;
+
+		Edge(int a, int b, int w) {
+			this.a = a;
+			this.b = b;
+			this.w = w;
+		}
+
+		public int compareTo(Edge o) {
+			return Integer.compare(this.w, o.w);
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
 		
-		p = new int[V+1];
+		V = Integer.parseInt(st.nextToken());
+		E = Integer.parseInt(st.nextToken());
+
+		p = new int[V + 1];
 		Arrays.fill(p, -1);
 		
-		long[][] l = new long[E][3];
-		
-		for (int i=0; i<E; i++) {
+		list = new Edge[E];
+		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			for (int j=0;j<3;j++)
-				l[i][j] = Long.parseLong(st.nextToken());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			list[i] = new Edge(a, b, w);
 		}
-		
-		Arrays.sort(l, ((l1, l2) -> Long.compare(l1[2], l2[2])));
-		
-		int cnt=0;
-		long ANS = 0;
-		for (long[] line : l) {
-			int a = (int)line[0];
-			int b = (int)line[1];
-			int aRoot = find(a);
-			int bRoot = find(b);
-			
-			if (aRoot == bRoot) continue;
-			
-			if (p[aRoot] < p[bRoot]) {
-				p[aRoot] = bRoot;
-			} else {
-				if (p[aRoot] == p[bRoot])  p[aRoot]--;
-				p[bRoot] = aRoot;
-			}
-			cnt++;
-			ANS += line[2];
-			if (cnt == V-1) break;
-		}
-		
-		System.out.println(ANS);
+		Arrays.sort(list);
 
+		int cnt = 0;
+		ANS = 0;
+		for (Edge e : list) {
+			if (union(e.a, e.b)) {
+				cnt++;
+				ANS += e.w;
+				if (cnt == V - 1)
+					break;
+			}
+		}
+
+		System.out.println(ANS);
 	}
+
 	static int find(int a) {
-		if (p[a] < 0) return a;
+		if (p[a] < 0)
+			return a;
 		return p[a] = find(p[a]);
+	}
+
+	static boolean union(int a, int b) {
+		int ar = find(a);
+		int br = find(b);
+
+		if (ar == br)
+			return false;
+
+		if (p[ar] < p[br]) {
+			p[br] = ar;
+		} else {
+			if (p[ar] == p[br])
+				p[br]--;
+			p[ar] = br;
+		}
+		return true;
 	}
 }
