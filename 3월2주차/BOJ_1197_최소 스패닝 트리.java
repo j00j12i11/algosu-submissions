c5640c42bd4f6428f86e84fbcd4cbdda2e800a5d
JIIN <73988273+j00j12i11@users.noreply.github.com>
2026-03-11 14:57:51 +0900

    Submit: 최소 스패닝 트리 (java)

--- 3월2주차/BOJ_1197_최소 스패닝 트리.java
+++ 3월2주차/BOJ_1197_최소 스패닝 트리.java
@@ -1,23 +1,16 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
-
-	static int V, E;
-	static long ANS;
-	static int[] p;
-	static Edge[] list;
-
-	static class Edge implements Comparable<Edge> {
-		int a, b, w;
-
-		Edge(int a, int b, int w) {
-			this.a = a;
-			this.b = b;
+public class Solution {
+	
+	static class Node implements Comparable<Node> {
+		int v, w;
+		Node(int v, int w) {
+			this.v = v;
 			this.w = w;
 		}
 
-		public int compareTo(Edge o) {
+		public int compareTo(Node o) {
 			return Integer.compare(this.w, o.w);
 		}
 	}
@@ -25,57 +18,51 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		V = Integer.parseInt(st.nextToken());
-		E = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(st.nextToken());
 
-		p = new int[V + 1];
-		Arrays.fill(p, -1);
-		
-		list = new Edge[E];
-		for (int i = 0; i < E; i++) {
+		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int w = Integer.parseInt(st.nextToken());
-			list[i] = new Edge(a, b, w);
-		}
-		Arrays.sort(list);
-
-		int cnt = 0;
-		ANS = 0;
-		for (Edge e : list) {
-			if (union(e.a, e.b)) {
-				cnt++;
-				ANS += e.w;
-				if (cnt == V - 1)
-					break;
+			int V = Integer.parseInt(st.nextToken());
+			int E = Integer.parseInt(st.nextToken());
+			
+			ArrayList<Node>[] adjList = new ArrayList[V+1];
+			for (int i=1;i<=V;i++) {
+				adjList[i] = new ArrayList<Node>();
 			}
+			
+			for (int i = 0; i < E; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				int w = Integer.parseInt(st.nextToken());
+				
+				adjList[a].add(new Node(b, w));
+				adjList[b].add(new Node(a, w));
+			}
+			
+			long ANS = 0;
+			PriorityQueue<Node> pq = new PriorityQueue<>();
+			boolean[] visited = new boolean[V+1];
+			
+			pq.add(new Node(1,0));
+			int cnt = V;
+			while(cnt > 0 && !pq.isEmpty()) {
+				Node cur = pq.poll();
+				
+				if (visited[cur.v]) continue;
+				cnt--;
+				ANS += cur.w;
+				visited[cur.v] = true;
+				
+				for (Node n : adjList[cur.v]) {
+					if (!visited[n.v]) {
+						pq.add(n);
+					}
+				}
+			}
+			
+			System.out.printf("#%d %d\n", tc, ANS);
 		}
 
-		System.out.println(ANS);
-	}
-
-	static int find(int a) {
-		if (p[a] < 0)
-			return a;
-		return p[a] = find(p[a]);
-	}
-
-	static boolean union(int a, int b) {
-		int ar = find(a);
-		int br = find(b);
-
-		if (ar == br)
-			return false;
-
-		if (p[ar] < p[br]) {
-			p[br] = ar;
-		} else {
-			if (p[ar] == p[br])
-				p[br]--;
-			p[ar] = br;
-		}
-		return true;
 	}
 }
